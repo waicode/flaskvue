cd8c54e6b743f7f9f8c365ffb474045911d18641
waicode <waicode37@gmail.com>
2021-05-11 23:22:59 +0900

    feat: add top

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def show():
+    return render_template('index.html')
+
+def __app_run():
+    app_port = 5000
+    app.run(host='0.0.0.0', port=app_port, debug=True)
+
+if __name__ == '__main__':
+    __app_run()
--- flaskvue/app/main.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from flaskvue import app
-
-@app.route('/', methods=['GET'])
-def show():
-    return render_template('index.html')
