525182eba7926acd285065d3b9cd829ce4ed02a4
waicode <waicode37@gmail.com>
2021-05-15 22:36:51 +0900

    feat: add flask message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def show():
-    return render_template('index.html')
+    return render_template('index.html', message='Main Flask message')
 
 def __app_run():
     app_port = 5000
